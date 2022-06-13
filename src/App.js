a7ea61b9eb64a88cdc8fab6b0ed592b6f38b72d5
Ali Alfredji <alfredjiali@gmail.com>
2022-06-13 18:53:58 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+    wrapper: {
+        height: '100vh',
+        width: '100%',
+        // backgroundColor: 'green',
+    },
+    userWrapper: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+    },
 }
 
-export default App;
+const App = () => {
+    return (
+        <div style={styles.wrapper}>
+            <div style={styles.userWrapper}>
+                Create your UserCard
+            </div>
+        </div>
+    )
+}
+
+export default App
